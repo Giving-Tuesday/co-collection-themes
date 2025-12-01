ad33f399b584c843bf772d8768f2cde872e43e54
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2025-12-01 13:38:46 -0500

    [0.3.7 fixes] add white color to IconLabel

--- src/IconLabel/index.jsx
+++ src/IconLabel/index.jsx
@@ -1,12 +1,12 @@
 import { findIcon, findIconByLabel } from '../settings/ICON_LIST';
 import styles from './IconLabel.module.css';
 
-const IconLabel = ({ as = 'p', icon, label }) => {
+const IconLabel = ({ as = 'p', icon, label, type = 'primary' }) => {
   const Icon = icon ? findIcon(icon) : findIconByLabel(label);
   const Element = as;
 
   return (
-    <Element className={styles.iconLabel}>
+    <Element className={`${styles.iconLabel} ${styles[type]}`}>
       <span className={styles.iconContainer}>
         {Icon && <Icon className={styles.icon} />}
       </span>
