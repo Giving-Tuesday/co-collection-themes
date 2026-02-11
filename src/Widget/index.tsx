9eb60d8a8c67b62a2ceaa25d3b0d9bc4ba0ba241
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2026-02-11 10:21:42 -0500

    [ts-migrate-build] - remove propTypes, no longer required since using TypeScript - modify package scripts to use typescript rollup config file - revert rollup imports to older functioning syntax, ensure config matches tsconfig - add basic declaration file for module.css files - resolve typescript errors in modules

--- src/Widget/index.tsx
+++ src/Widget/index.tsx
@@ -1,22 +1,18 @@
 import { getThemeCard } from '../utils/card-themes.utils';
 import styles from './Widget.module.css';
 
-const Widget = ({
-  items,
-  widgetTheme,
-  embedUrl,
-  maxCardsPerRow = 5
-}: any) => {
+const Widget = ({ items, widgetTheme, embedUrl }: any) => {
   const CardComponent = getThemeCard(widgetTheme);
-  const itemCount = items?.length || 0;
 
   return (
     <div className={`daro-widget ${styles.base} ${styles.widgetWrapper}`}>
-      {items?.map((item: any) => <CardComponent
-        key={item._id}
-        href={`${embedUrl}?co-item=${item.slug}&from=widget`}
-        item={item}
-      />)}
+      {items?.map((item: any) => (
+        <CardComponent
+          key={item._id}
+          href={`${embedUrl}?co-item=${item.slug}&from=widget`}
+          item={item}
+        />
+      ))}
     </div>
   );
 };
