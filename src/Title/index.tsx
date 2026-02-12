b1328a88c7e8947ac01086c3fea7341badd5e441
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2026-02-11 20:46:42 -0500

    [modal-dialog] update Title and ReadMore components, moving container to separate wrapper component

--- src/Title/index.tsx
+++ src/Title/index.tsx
@@ -1,30 +1,57 @@
-import type { ElementType, CSSProperties, HTMLAttributes } from 'react';
+import type {
+  ElementType,
+  CSSProperties,
+  HTMLAttributes,
+  Dispatch,
+  SetStateAction,
+  ReactNode,
+} from 'react';
 import styles from './title.module.css';
 import clsx from 'clsx';
+import type { Item } from '../types';
 
 interface TitleProps extends HTMLAttributes<HTMLParagraphElement> {
   CustomLink?: ElementType | undefined;
   href: string;
   text: string;
   height?: CSSProperties['height'];
+  setItem?: (() => void) | undefined;
 }
-
-const Title = ({ className, CustomLink, height, href, text }: TitleProps) => {
+const TitleWrapper = ({
+  className,
+  height,
+  children,
+  onClick,
+}: {
+  children: ReactNode;
+  height?: CSSProperties['height'];
+} & HTMLAttributes<HTMLParagraphElement>) => {
   return (
-    <p className={clsx(styles.title, className)} style={{ height }}>
-      {CustomLink ? (
+    <div className={clsx(styles.title, className)} style={{ height }} onClick={onClick}>
+      {children}
+    </div>
+  );
+};
+
+const Title = ({ className, CustomLink, height, href, text, setItem }: TitleProps) => {
+  if (setItem) {
+    return (
+      <TitleWrapper className={className} height={height} onClick={() => setItem()}>
+        <span>{text}</span>
+      </TitleWrapper>
+    );
+  }
+  if (CustomLink) {
+    return (
+      <TitleWrapper className={className} height={height}>
         <CustomLink href={href}>{text}</CustomLink>
-      ) : (
-        <a
-          href={href}
-          className={
-            'link' in styles && typeof styles.link === 'string' ? styles.link : undefined
-          }
-        >
-          {text}
-        </a>
-      )}
-    </p>
+      </TitleWrapper>
+    );
+  }
+  return (
+    <TitleWrapper className={className} height={height}>
+      <a href={href}>{text}</a>
+    </TitleWrapper>
   );
 };
 
