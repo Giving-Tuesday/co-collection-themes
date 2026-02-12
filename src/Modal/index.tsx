3eccff41a32d6f99b03526b196d10bbda67bc745
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2026-02-11 20:51:43 -0500

    [modal] update colours and hide title / description

--- src/Modal/index.tsx
+++ src/Modal/index.tsx
@@ -2,20 +2,25 @@ import * as Dialog from '@radix-ui/react-dialog';
 import styles from './Modal.module.css';
 import { RxCross2 } from 'react-icons/rx';
 import type { ReactNode } from 'react';
+import * as VisuallyHidden from '@radix-ui/react-visually-hidden';
 
 interface ModalProps {
   open: boolean;
   onOpenChange: (value: boolean) => void;
   children: ReactNode;
-  title?: string;
+  title: string;
+  description: string;
 }
 
-const Modal = ({ open, onOpenChange, children, title }: ModalProps) => (
+const Modal = ({ open, onOpenChange, children, title, description }: ModalProps) => (
   <Dialog.Root open={open} onOpenChange={onOpenChange}>
     <Dialog.Portal>
       <Dialog.Overlay className={styles.Overlay} />
       <Dialog.Content className={styles.Content}>
-        {title && <Dialog.Title className={styles.Title}>{title}</Dialog.Title>}
+        <VisuallyHidden.Root>
+          <Dialog.Title>{title}</Dialog.Title>
+          <Dialog.Description>{description}</Dialog.Description>
+        </VisuallyHidden.Root>
         <div className={styles.ScrollArea}>{children}</div>
         <Dialog.Close asChild>
           <button className={styles.IconButton} aria-label="Close">
