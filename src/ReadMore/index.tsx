2ed42c621c6d1a135cf9b6c4d83f06854619647f
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2026-02-11 20:46:42 -0500

    [modal-dialog] update Title and ReadMore components, moving container to separate wrapper component

--- src/ReadMore/index.tsx
+++ src/ReadMore/index.tsx
@@ -1,4 +1,10 @@
-import type { ElementType } from 'react';
+import type {
+  Dispatch,
+  ElementType,
+  HTMLAttributes,
+  ReactNode,
+  SetStateAction,
+} from 'react';
 import styles from './readMore.module.css';
 
 interface ReadMoreProps {
@@ -6,31 +12,58 @@ interface ReadMoreProps {
   href: string;
   label?: string;
   align?: 'right';
+  setItem?: (() => void) | undefined;
 }
+
+const ReadMoreWrapper = ({
+  children,
+  align = 'right',
+  onClick,
+}: { children: ReactNode; align?: 'right' } & HTMLAttributes<HTMLDivElement>) => {
+  const justify = align === 'right' ? 'flex-end' : 'flex-start';
+  return (
+    <div className={styles.wrapper} style={{ justifyContent: justify }} onClick={onClick}>
+      {children}
+    </div>
+  );
+};
 
 const ReadMore = ({
   CustomLink,
   href,
   label = 'Read More',
   align = 'right',
+  setItem,
 }: ReadMoreProps) => {
-  const justify = align === 'right' ? 'flex-end' : 'flex-start';
-
-  return (
-    <div className={styles.wrapper} style={{ justifyContent: justify }}>
-      {CustomLink ? (
+  const handleClick = () => {
+    if (!setItem) return;
+    setItem();
+  };
+  if (setItem) {
+    return (
+      <ReadMoreWrapper align={align} onClick={handleClick}>
+        <p>{label}</p>
+      </ReadMoreWrapper>
+    );
+  }
+  if (CustomLink) {
+    return (
+      <ReadMoreWrapper align={align}>
         <CustomLink href={href}>{label}</CustomLink>
-      ) : (
-        <a
-          href={href}
-          className={
-            'link' in styles && typeof styles.link === 'string' ? styles.link : undefined
-          }
-        >
-          {label}
-        </a>
-      )}
-    </div>
+      </ReadMoreWrapper>
+    );
+  }
+  return (
+    <ReadMoreWrapper align={align}>
+      <a
+        href={href}
+        className={
+          'link' in styles && typeof styles.link === 'string' ? styles.link : undefined
+        }
+      >
+        {label}
+      </a>
+    </ReadMoreWrapper>
   );
 };
 
