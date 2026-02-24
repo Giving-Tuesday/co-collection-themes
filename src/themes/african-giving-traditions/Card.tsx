08b622c27506b3ce216405668a654cbb2410fed4
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2026-02-24 14:43:53 -0500

    [african-giving-theme] CSS

--- src/themes/african-giving-traditions/Card.tsx
+++ src/themes/african-giving-traditions/Card.tsx
@@ -7,6 +7,8 @@ import { truncateText } from '../../utils/text.utils';
 import theme from './theme.module.css';
 import styles from './Card.module.css';
 import type { CardProps } from '../../types';
+import { Author } from '../../Author';
+import clsx from 'clsx';
 
 const Card = ({ CustomLink, href, item, setItem }: CardProps) => {
   const { author, desc, title } = item;
@@ -17,18 +19,14 @@ const Card = ({ CustomLink, href, item, setItem }: CardProps) => {
       <CardContent align="end" position="top">
         <Badge>{location}</Badge>
       </CardContent>
-      <CardContent position="middle">
-        <Title
-          {...{ CustomLink, href, setItem }}
-          text={truncateText(title, 40)}
-          height="52px"
-        />
-        <div className={styles.authorContainer}>
-          {item.author ? (
-            <p className={styles.author}>
-              by {author ? truncateText(author.join(', '), 100) : 'Unknown Author'}
-            </p>
-          ) : null}
+      <CardContent position="middle" align="start">
+        <div className={styles.authorTitleContainer}>
+          <Title
+            {...{ CustomLink, href, setItem }}
+            text={truncateText(title, 60)}
+            className={clsx(styles.title, Author({ author }) && styles.titleWithAuthor)}
+          />
+          <Author author={author} />
         </div>
         {desc ? (
           <div
